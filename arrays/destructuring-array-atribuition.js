d8304e01be8b2b2f2b544ae4fd06b5e134d2d1d5
Weslley Stein <weslley.stein@gmail.com>
2020-11-29 15:59:55 -0300

    Lesson Complete

---
+++ arrays/destructuring-array-atribuition.js
@@ -0,0 +1,3 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const [firstNumber, secondNumber] = numbers;
+//look how this work, you are taking each value of -numbers- and puting in sort on "firstNumber, secondNumber"
