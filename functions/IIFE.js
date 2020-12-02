8be1fde8367b7c8106d74d4e86bf927cd4c1af14
Weslley Stein <weslley.stein@gmail.com>
2020-12-02 00:12:24 -0300

    finished IIFE Lesson and Started factory-functions lesson

---
+++ functions/IIFE.js
@@ -0,0 +1,9 @@
+//IIFE = Immediately Invoked Function Expression
+(function(){
+    const nome = 'luiz'
+})();
+
+//That is useful to protect some Variables and Expressions with Function Scope
+
+const nome = "pedro"
+console.log(nome)
